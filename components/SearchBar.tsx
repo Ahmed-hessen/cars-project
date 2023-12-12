6310fe673b18cc829991ea6959d494aac8cf4d0f
Ahmed-hessen <ahmedhessen2194@gmail.com>
2023-12-12 23:10:41 +0200

    third commit

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -54,7 +54,7 @@ const SearchBar = () => {
       <div className="searchbar__item">
         <SearchManufacturer
           manufacturer={manufacturer}
-          setManufacturer={setManufacturer}
+          setManuFacturer={setManufacturer}
         />
         <SearchButton otherClasses="sm:hidden" />
       </div>
